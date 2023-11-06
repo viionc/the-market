db4b30b8bcc2ed144125f0b846c53b84fad00aa1
viionc <marek.d.stankiewicz@gmail.com>
2023-11-06 12:16:36 +0100

    more testing

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const connectDB = require("../config/db");
 const {startTickInterval, stopTickInterval} = require("../tickHandler");
 
 connectDB();
-
 startTickInterval();
 
+const whitelist = ["https://the-market-beryl.vercel.app/", "http://localhost:5173/"];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: true}));
 app.use("/api/listings", require("../routes/dataRoute"));
 app.use("/api/auth", require("../routes/authRoute"));
